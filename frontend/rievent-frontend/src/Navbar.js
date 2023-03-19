f0716904c4f1de01832c58a0670c9e6fa446251a
Matteo Plišić <77995521+MatteoPlisic@users.noreply.github.com>
2023-03-19 19:07:27 +0000

    Adding a registration form

--- frontend/rievent-frontend/src/Navbar.js
+++ frontend/rievent-frontend/src/Navbar.js
@@ -28,6 +28,86 @@ function LoginForm({ onLogin }) {
   );
 }
 
+function RegistrationForm() {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [birthdate, setBirthdate] = useState("");
+  const [gender, setGender] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Submit form data to server
+  };
+
+  return (
+    <div className="container">
+      <h1>Registration Form</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="fullName">Full Name</label>
+        <input
+          type="text"
+          id="fullName"
+          name="fullName"
+          placeholder="Enter your full name"
+          value={fullName}
+          onChange={(event) => setFullName(event.target.value)}
+        />
+
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Enter your email address"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+
+        <label htmlFor="confirmPassword">Confirm Password</label>
+        <input
+          type="password"
+          id="confirmPassword"
+          name="confirmPassword"
+          placeholder="Confirm your password"
+          value={confirmPassword}
+          onChange={(event) => setConfirmPassword(event.target.value)}
+        />
+
+        <label htmlFor="birthdate">Date of Birth</label>
+        <input
+          type="date"
+          id="birthdate"
+          name="birthdate"
+          value={birthdate}
+          onChange={(event) => setBirthdate(event.target.value)}
+        />
+
+        <label htmlFor="gender">Gender</label>
+        <select id="gender" name="gender" value={gender} onChange={(event) => setGender(event.target.value)}>
+          <option value="">Select your gender</option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+          <option value="other">Other</option>
+        </select>
+
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
 function HomeContent() {
   return (
     <div className='Home-content'>
@@ -40,20 +120,35 @@ function HomeContent() {
 function App() {
   const [showLoginForm, setShowLoginForm] = useState(false);
   const [showHomeContent, setShowHomeContent] = useState(true);
-
+  const [showRegistrationForm, setShowRegistrationForm] = useState(false);
   const handleLoginClick = () => {
     setShowLoginForm(true);
     setShowHomeContent(false);
+    setShowRegistrationForm(false)
   };
 
   const handleLogin = () => {
     setShowLoginForm(false);
     setShowHomeContent(false);
+    setShowRegistrationForm(false)
   };
+
+  const handleRegistration = () => {
+    setShowLoginForm(false);
+    setShowHomeContent(false);
+    setShowRegistrationForm(false)
+  }
+
+  const handleRegisterClick = () => {
+    setShowLoginForm(false);
+    setShowHomeContent(false);
+    setShowRegistrationForm(true)
+  }
 
   const handleHomeClick = () => {
     setShowLoginForm(false);
     setShowHomeContent(true);
+    setShowRegistrationForm(false);
   };
 
   return (
@@ -65,7 +160,7 @@ function App() {
               <a href="#" onClick={handleHomeClick}>Home</a>
             </li>
             <li className="nav-item">
-              <a href="#">Register</a>
+              <a href="#" onClick={handleRegisterClick}>Register</a>
             </li>
             <li className="nav-item">
               <a href="#" onClick={handleLoginClick}>Login</a>
@@ -75,6 +170,7 @@ function App() {
       </header>
       {showLoginForm && <LoginForm onLogin={handleLogin} />}
       {showHomeContent && <HomeContent />}
+      {showRegistrationForm && <RegistrationForm onRegistration={handleRegistration}/>}
     </div>
   );
 }
--- frontend/rievent-frontend/src/index.js
+++ frontend/rievent-frontend/src/index.js
@@ -11,7 +11,7 @@ root.render(
   
   <React.StrictMode>
     <Navbar />
-  <h1>hi</h1>
+  
   </React.StrictMode>,
 
 
