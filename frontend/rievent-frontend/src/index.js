ccd1bcd284806737d8597f96c4b14bf8b487d07a
Matteo Plišić <77995521+MatteoPlisic@users.noreply.github.com>
2023-03-20 16:43:44 +0000

    Small modifications to Navbar.css

--- frontend/rievent-frontend/src/index.js
+++ frontend/rievent-frontend/src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
 import reportWebVitals from './reportWebVitals';
-
 import Navbar from './Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
