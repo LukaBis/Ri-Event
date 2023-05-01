182932fc42db5bdf6ab514ea7c9aa2518fa8b03c
Patrik <paunovicpatrik007@gmail.com>
2023-05-01 22:15:07 +0200

    fixes #74

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { BeatLoader } from 'react-spinners';
-import Navbar from '../Navbar';
+import Navbar from './navbar/Navbar';
 import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Navbar from './Navbar';
+import Navbar from './components/navbar/Navbar';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
