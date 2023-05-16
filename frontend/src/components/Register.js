3cc607a68e386de0d7effb207d05db49c5c1b204
matteo <mplisic@riteh.hr>
2023-05-16 02:24:29 +0200

    Fixes #51

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,94 +1,122 @@
+
 import React, { useState } from 'react';
-import { Link, redirect } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import axios from 'axios';
-import Navbar from './Navbar/Navbar';
 import Cookies from 'js-cookie';
-import { useNavigate } from 'react-router-dom';
+import Navbar from './Navbar/Navbar';
+import { Typography, Button } from '@mui/material';
+import { makeStyles, styled, useTheme } from '@mui/styles';
+import { TextField } from '@mui/material/';
+import CustomTextField from '../styles/CustomTextField.js';
+import useStyles from '../styles/UseStyles';
 
 const Register = () => {
-    const [fullName, setFullName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
-    const Navigate = useNavigate('');
+  const classes = useStyles();
 
-    axios.defaults.withCredentials = true;
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        
-        try {
-            // Fetch CSRF token
-            const response = await axios.get('/sanctum/csrf-cookie', {
-                withCredentials: true,
-            });
+  axios.defaults.withCredentials = true;
 
-            // Submit form data to server
-            await axios.post('/register', {
-                name: fullName,
-                email: email,
-                password: password,
-                password_confirmation: confirmPassword,
-                }, {
-                headers: {
-                    'X-XSRF-TOKEN': Cookies.get('XSRF-TOKEN'),
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                },
-            });
-            
-            Navigate('/');
-        } catch (error) {
-            console.error(error);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      // Fetch CSRF token
+      const response = await axios.get('http://localhost/sanctum/csrf-cookie', {
+        withCredentials: true,
+      });
+
+      // Submit form data to server
+      await axios.post(
+        '/register',
+        {
+          name: fullName,
+          email: email,
+          password: password,
+          password_confirmation: confirmPassword,
+        },
+        {
+          headers: {
+            'X-XSRF-TOKEN': Cookies.get('XSRF-TOKEN'),
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
         }
-    };
+      );
 
-    return (
-    <><Navbar /><div className="Register-div">
+      //history.push('/');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className={classes.form}>
+        <Typography variant="h4" gutterBottom align='center' fontFamily='Roboto '>
+          Register
+        </Typography>
         <form onSubmit={handleSubmit}>
-        <label htmlFor="fullName">Full Name</label>
-        <input
-            type="text"
+          <CustomTextField
+            className={classes.textField}
+            label="Full Name"
+            variant="outlined"
             id="fullName"
             name="fullName"
-            placeholder="Enter your full name"
             value={fullName}
-            onChange={(event) => setFullName(event.target.value)} />
-
-        <label htmlFor="email">Email</label>
-        <input
+            onChange={(event) => setFullName(event.target.value)}
+            marginbottom={classes.textField}
+          />
+          <CustomTextField
+            className={classes.textField}
+            label="Email"
+            variant="outlined"
             type="email"
             id="email"
             name="email"
-            placeholder="Enter your email address"
             value={email}
-            onChange={(event) => setEmail(event.target.value)} />
-
-        <label htmlFor="password">Password</label>
-        <input
+            onChange={(event) => setEmail(event.target.value)}
+            sx={{ marginbottom: '0px' }}
+          />
+          <CustomTextField
+            className={classes.textField}
+            label="Password"
+            variant="outlined"
             type="password"
             id="password"
             name="password"
-            placeholder="Enter your password"
             value={password}
-            onChange={(event) => setPassword(event.target.value)} />
-
-        <label htmlFor="confirmPassword">Confirm Password</label>
-        <input
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <CustomTextField
+            className={classes.textField}
+            label="Confirm Password"
+            variant="outlined"
             type="password"
             id="confirmPassword"
             name="confirmPassword"
-            placeholder="Confirm your password"
             value={confirmPassword}
-            onChange={(event) => setConfirmPassword(event.target.value)} />
-
-        <button type="submit">Register</button>
-        <p>
-            <Link to="/login">Already have an account? Log in!</Link>
-        </p>
+            onChange={(event) => setConfirmPassword(event.target.value)}
+          />
+          <Button
+            className={classes.submitButton}
+            type="submit"
+            variant="contained"
+            color="primary"
+          >
+            Register
+          </Button>
         </form>
-    </div></>
-    );
+        <Typography variant="subtitle1" align='center'>
+          Already have an account? <Link to="/login">Log in</Link>
+        </Typography>
+      </div>
+    </>
+  );
 };
 
 export default Register;
