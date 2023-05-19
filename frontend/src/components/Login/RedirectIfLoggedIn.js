0fd39392ccff226120f1239c279390faf7cefa4f
matteo <mplisic@riteh.hr>
2023-05-19 03:00:34 +0200

    Small modifications

--- frontend/src/components/Login/RedirectIfLoggedIn.js
+++ frontend/src/components/Login/RedirectIfLoggedIn.js
@@ -2,20 +2,24 @@ import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
 
+function checkLoggedIn () {
+  if (Cookies.get('XSRF-TOKEN')) {
+    return true;
+  }
+  return false;
+};
+
 function RedirectIfLoggedIn({ }) {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const checkLoggedIn = async () => {
-      if (Cookies.get('XSRF-TOKEN')) {
-        navigate('/');
-      }
-    };
-
-    checkLoggedIn();
+   const isLoggedin = checkLoggedIn();
+   if(isLoggedin === true){
+    navigate('/')
+   }
   }, [navigate]);
 
-  return null; // Since this component doesn't render anything, return null
+  return null; 
 }
 
 export default RedirectIfLoggedIn;
