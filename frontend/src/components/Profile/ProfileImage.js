e925de8b2fe59702816ae2e137b00280558085cc
Luka8st <luka.bursic5@gmail.com>
2023-05-25 01:15:55 +0200

    added functionality for deleting profile images

--- frontend/src/components/Profile/ProfileImage.js
+++ frontend/src/components/Profile/ProfileImage.js
@@ -1,37 +1,97 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, Typography } from '@mui/material';
 import Button from '@mui/material/Button';
 import useProfileImage from './useProfileImage';
 import uploadProfileImage from '../../requests/put/uploadProfileImage';
 import Alert from '@mui/material/Alert';
+import deleteProfileImage from '../../requests/delete/deleteProfileImage';
+import Modal from '@mui/material/Modal';
+import styles from './styles';
 
 function ProfileImage() {
 
     let profileImage = useProfileImage();
     const [uploadedFile, setUploadedFile] = useState(null);
-    const [successAlert, setSuccessAlert] = useState(false);
+    const [successAlert, setSuccessAlert] = useState(null);
     const [errorAlert, setErrorAlert] = useState(null);
+    const [modalOpen, setModalOpen] = useState(false);
+    const [disabled, setDisabled] = useState(false);
 
     const handleFileChange = (event) => {
         const selectedFile = event.target.files[0];
         setUploadedFile(selectedFile);
     };
 
+    const handleFileDelete = () => {
+        setDisabled(true);
+        deleteProfileImage()
+            .then(res => {
+                setSuccessAlert(res.message);
+                setErrorAlert(null);
+
+                setUploadedFile(null);
+            })
+            .catch(err => {
+                setErrorAlert(err.message);
+                setSuccessAlert(false);
+            })
+            .finally(() => {
+                setModalOpen(false);
+                setDisabled(false);
+            })
+    };
+
     const handleFileUpload = () => {
         uploadProfileImage(uploadedFile)
-        .then(res => {
-            setSuccessAlert(res.message);
-            setErrorAlert(null);
-        })
-        .catch(err => {
-            setErrorAlert(err.message);
-            setSuccessAlert(false);
-        })
-    };        
+            .then(res => {
+                setSuccessAlert(res.message);
+                setErrorAlert(null);
+            })
+            .catch(err => {
+                setErrorAlert(err.message);
+                setSuccessAlert(false);
+            })
+            .finally(() => {
+                document.getElementById('image').value = null;
+            })
+    };
 
     return (
         <>
             <Box sx={{ m: 10 }}>
+                <Modal
+                    open={modalOpen}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Box sx={styles.modal}>
+                        <Typography id="modal-modal-title" variant="h6" component="h3">
+                            Are you sure you want to delete this image?
+                        </Typography>
+                        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                            <Button
+                                variant="outlined"
+                                onClick={() => setModalOpen(false)}
+                                sx={{ mt: 4 }}
+                                disabled={disabled}
+                            >
+                                Cancel
+                            </Button>
+                            <Button
+                                variant="outlined"
+                                onClick={handleFileDelete}
+                                sx={{ mt: 4, ml: 2, borderColor: 'red', color: 'red', 
+                                '&:hover': {
+                                    borderColor: 'red',
+                                }, }}
+                                disabled={disabled}
+                            >
+                                Delete
+                            </Button>
+                        </Typography>
+                    </Box>
+                </Modal>
+
                 <Typography variant='h3' sx={{ mb: 5 }}>
                     Profile Image
                 </Typography>
@@ -59,17 +119,29 @@ function ProfileImage() {
                     )
                 }
                 <label>Upload new profile image</label>
-                <input type='file' onChange={handleFileChange} /> <br />
-                <Button 
+                <input id='image' type='file' onChange={handleFileChange} /> <br />
+                <Button
                     variant="outlined"
                     onClick={handleFileUpload}
                     sx={{ mt: 4 }}>
-                        Update
+                    Update
+                </Button>
+                <br/>
+                <Button
+                    variant="outlined"
+                    onClick={() => setModalOpen(true)}
+                    sx={{
+                        mt: 2, borderColor: 'red', color: 'red', 
+                        '&:hover': {
+                            borderColor: 'red',
+                        },
+                    }}>
+                    Delete
                 </Button>
 
                 {successAlert ? (
                     <Alert severity="success" sx={{ mt: 4 }}>
-                        Successful update!
+                        {successAlert}
                     </Alert>
                 ) : null}
 
--- frontend/src/components/Profile/styles.js
+++ frontend/src/components/Profile/styles.js
@@ -9,6 +9,19 @@ const styles = {
     },
     alert: {
         width: '280px',
+    },
+    modal: {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000 rounded',
+        borderRadius: '10px',
+        boxShadow: 24,
+        p: 4,
+        color: 'black'
     }
 };
 
