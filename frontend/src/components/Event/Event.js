5d64611f3a2581d5e0b2411e12469f71accbcacc
Luka8st <luka.bursic5@gmail.com>
2023-05-29 10:01:18 +0200

    added maps api for single event

--- frontend/src/components/Event/Event.js
+++ frontend/src/components/Event/Event.js
@@ -1,21 +1,37 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Typography from '@mui/material/Typography';
 import { Box } from '@mui/system';
 import getSingleEvent from '../../requests/get/getSingleEvent';
 import './event.css';
+import { GoogleMap, MarkerF, useLoadScript } from "@react-google-maps/api";
 
 function Event() {
 
     const { eventId } = useParams();
     const [event, setEvent] = useState({});
+    const [center, setCenter] = useState(null);
+
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: process.env.REACT_APP_MAPS_API_KEY,
+    });
 
     useEffect(() => {
         getSingleEvent(eventId).then(event => {
             setEvent(event);
+
+            setCenter({lat: event.latitude, lng: event.longitude})
         })
     }, []);
 
+    function Map() {
+        return <GoogleMap 
+            zoom={13} 
+            center={center} 
+            mapContainerClassName='map-container'>
+                <MarkerF position={center}/>
+        </GoogleMap>
+    }
     return (
         <>
             <Box className='event-container'>
@@ -25,9 +41,9 @@ function Event() {
 
                 <Box marginBottom={2} className='evet-image-container'>
                     <img
-                    src="https://media.istockphoto.com/id/868935172/photo/heres-to-tonight.jpg?s=612x612&w=0&k=20&c=v1ceJ9aZwI43rPaQeceEx5L6ODyWFVwqxqpadC2ljG0="
-                    alt="Event"
-                    className='event-image'
+                        src="https://media.istockphoto.com/id/868935172/photo/heres-to-tonight.jpg?s=612x612&w=0&k=20&c=v1ceJ9aZwI43rPaQeceEx5L6ODyWFVwqxqpadC2ljG0="
+                        alt="Event"
+                        className='event-image'
                     />
                 </Box>
 
@@ -39,23 +55,31 @@ function Event() {
 
                 <Box marginBottom={2}>
                     <Typography variant="body1">
-                    <b>Host: </b> {event?.host}
+                        <b>Host: </b> {event?.host}
                     </Typography>
                 </Box>
 
                 <Box marginBottom={2}>
                     <Typography variant="body1">
-                    <b>Event date: </b> {event?.date}
+                        <b>Event date: </b> {event?.date}
                     </Typography>
                 </Box>
 
                 <Box marginBottom={2}>
                     <Typography variant="body1">
-                    <b>Event start time: </b> {event?.start_time}
+                        <b>Event start time: </b> {event?.start_time}
                     </Typography>
                 </Box>
+
+                <Box marginBottom={2}>
+                <Typography variant="body1">
+                    <b>Event location: </b>
+                        {!isLoaded ? <>Loading...</> : <Map/>}
+                </Typography>
+                </Box>
+
             </Box>
-        </>
+        </> 
     )
 }
 
